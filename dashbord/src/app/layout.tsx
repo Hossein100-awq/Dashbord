3952e7912346764a70733a6d43f4720e1618bc5f
Hossein321a <hossein5800hossein@gmail.com>
2026-01-08 14:08:08 +0330

    ready for work

--- dashbord/src/app/layout.tsx
+++ dashbord/src/app/layout.tsx
@@ -1,7 +1,6 @@
 // src/app/layout.tsx
 import { Vazirmatn } from "next/font/google";
 import "./globals.css";
-import Script from "next/script";
 import React from "react";
 import ReactQueryProvider from "./../componnets/Provider";
 
@@ -20,23 +19,6 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fa" dir="rtl" className={vazir.variable}>
-      <head>
-        <Script id="theme-init" strategy="beforeInteractive">
-          {`(function(){
-            try{
-              var t = localStorage.getItem('theme');
-              if(t === 'dark'){ document.documentElement.classList.add('dark'); return; }
-              if(t === 'light'){ document.documentElement.classList.remove('dark'); return; }
-              // اصلاح شده: اگر تم ذخیره نشده، از تنظیمات سیستم استفاده کن
-              if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
-                document.documentElement.classList.add('dark');
-              } else {
-                document.documentElement.classList.remove('dark');
-              }
-            }catch(e){}
-          })();`}
-        </Script>
-      </head>
       <body>
         <ReactQueryProvider>
           {children}
